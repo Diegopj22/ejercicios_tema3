09002e60c1373bf05f1a6bd5e2bb4da4b322d8a2
Diegopj22 <dpj2202@gmail.com>
2022-11-06 17:53:20 +0100

    constructor

---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+class Nave:
+    def __init__(self,nombre,largo,trip,cant_pas):
+        self.nombre= nombre
+        self.largo= largo
+        self.trip= trip
+        self.cant_pas= cant_pas
+        
