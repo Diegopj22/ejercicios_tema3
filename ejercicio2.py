26fcce0be27b4f8655f3c4f283e35cf7ab8c5fb3
Diegopj22 <dpj2202@gmail.com>
2022-11-06 17:38:36 +0100

    sarrus

--- ejercicio2.py
+++ ejercicio2.py
@@ -4,3 +4,11 @@ def rellenar(n):
     for x in range(n):
         for y in range(n):
             matriz[x][y]=float(input('valor de ['+str(x)+'['+ str(y)+')='))
+
+def sarrus (n):
+    for z in range (n-1):
+        for x in range (1,n-z):
+            if (matriz[z][z]!=0):
+                p = matriz[x+z][z]/matriz[z][z]
+                for y in range(n):
+                    matriz[x+z][y]= matriz[x+z][y]-(matriz[z][y]*p)
