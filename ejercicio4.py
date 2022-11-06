d9e2b99695e125ecc5d3136e16559b775cf1f6d8
Diegopj22 <dpj2202@gmail.com>
2022-11-06 18:29:52 +0100

    restar

--- ejercicio4.py
+++ ejercicio4.py
@@ -49,3 +49,15 @@ class Polinomio(object):
             pol += signo + str(aux.info.valor) + "x**" + str(aux.info.termino)       
        else:
         return poli
+    
+    def restar (polinomio1,polinomio2):
+       paux = Polinomio()
+       if polinomio1> polinomio2:
+        mayor = polinomio1
+       else:
+        mayor = polinomio2
+       for i in range(0, mayor.grado +1):
+          total = polinomio1.obtener_valor(polinomio1, i) - polinomio2.obtener_valor(polinomio2, i)
+          if total != 0:
+            total.agregar_termino (paux, i , total)
+            return paux
