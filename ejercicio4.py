8b40198ae35fe38c68ed296a256d02bf20864ca1
Diegopj22 <dpj2202@gmail.com>
2022-11-06 18:19:19 +0100

    commit5

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,2 +1,9 @@
 class Nodo(object):
     info,sig = None, None 
+
+class datoPolinomio(object):
+
+    def __init__(self, valor, termino):
+        self.valor = valor
+        self.termino = termino
+
