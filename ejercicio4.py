7956eaea41342349787e54f4a7275f1d219c9fda
Diegopj22 <dpj2202@gmail.com>
2022-11-06 18:32:25 +0100

    dividir

--- ejercicio4.py
+++ ejercicio4.py
@@ -61,3 +61,20 @@ class Polinomio(object):
           if total != 0:
             total.agregar_termino (paux, i , total)
             return paux
+    
+    def dividir (polinomio1,polinomio2):
+        paux = Polinomio()
+        pol1 = polinomio1.termino_mayor
+        while pol1 is not None:
+           pol2 = polinomio2.termino_mayor
+           while pol2 is not None:
+              termino = pol1.info.termino + pol2.info.termino
+              valor = pol1.info.valor / pol2.info.valor
+              if valor.obtener_valor(paux, termino) != 0:
+                valor += valor.obtener_valor(paux, termino)
+                termino.modificar_termino (paux, termino, valor)
+              else:
+                termino.agregar_termino(paux, termino, valor)
+           pol2 = pol2.sig
+           pol1 = pol1.sig
+        return paux
